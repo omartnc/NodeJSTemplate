bde64431750e349434709d5bfbe8d8e7673c3209
Ömer TUNÇ <omer_tunc_04@hotmail.com>
2019-03-04 00:03:18 +0300

    update

--- middleware/admin.js
+++ middleware/admin.js
@@ -1,9 +1,21 @@
 
-module.exports = function (req, res, next) { 
-  // 401 Unauthorized
-  // 403 Forbidden 
-  
-  if (!req.user.isAdmin) return res.status(403).send('Access denied.');
 
-  next();
+
+const jwt = require('jsonwebtoken');
+const config = require('config');
+
+module.exports = function (req, res, next) {
+  const token = req.header('x-auth-token');
+  if (!token) return res.status(401).send('Access denied. No token provided.');
+
+  try {
+    const decoded = jwt.verify(token, config.get('jwtPrivateKey'));
+    req.user = decoded; 
+
+  if (!req.user.isAdmin) return res.status(403).send('Access denied.');
+    next();
+  }
+  catch (ex) {
+    res.status(400).send('Invalid token.');
+  }
 }
